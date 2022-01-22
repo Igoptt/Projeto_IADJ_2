be9c50a4a636e8e89cd9fe987b55d47e53904848
katherin.varela <katherinfatima@gmail.com>
2022-01-22 19:28:47 +0000

    teste

--- Assets/EOTS/Base.cs
+++ Assets/EOTS/Base.cs
@@ -7,6 +7,7 @@ namespace Assets.EOTS
 {
     public class Base : MonoBehaviour
     {
+        //teste
         public List<ISoldier> MemberOfTeamBlue;
         public List<ISoldier> MemberOfTeamRed;
 
